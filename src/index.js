8df01e57090fb1e11299acb53429c353b2e0c2d7
Carlos Osorio <osorio.uis@gmail.com>
2022-08-14 16:41:28 -0500

    Add babel loader to use JSX

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render('Hola jelou', document.body)
+ReactDOM.render(<h1>Hola jelou luti</h1>, document.getElementById('app'))
